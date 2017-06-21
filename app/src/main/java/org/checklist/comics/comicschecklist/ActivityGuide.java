4f045ac72c3f8bec56535134a9002fc73c6d348a
FB <francesco.bevilacqua@bepatient.com>
2017-06-21 11:26:37 +0200

    Added images on app intro

--- app/src/main/java/org/checklist/comics/comicschecklist/ActivityGuide.java
+++ app/src/main/java/org/checklist/comics/comicschecklist/ActivityGuide.java
@@ -24,42 +24,37 @@ public class ActivityGuide extends AppIntro {
         // Instead of fragments, you can also use our default slide
         addSlide(AppIntroFragment.newInstance(getResources().getString(R.string.activity_help_home),
                 getResources().getString(R.string.activity_help_home_text),
-                R.mipmap.ic_launcher,
+                R.drawable.intro_main,
                 ContextCompat.getColor(this, R.color.primary)));
 
         addSlide(AppIntroFragment.newInstance(getResources().getString(R.string.activity_help_list),
                 getResources().getString(R.string.activity_help_list_text),
-                R.mipmap.ic_launcher,
+                R.drawable.intro_editori,
                 ContextCompat.getColor(this, R.color.primary)));
 
         addSlide(AppIntroFragment.newInstance(getResources().getString(R.string.activity_help_detail),
                 getResources().getString(R.string.activity_help_detail_text),
-                R.mipmap.ic_launcher,
+                R.drawable.intro_dettagli,
                 ContextCompat.getColor(this, R.color.primary)));
 
-        addSlide(AppIntroFragment.newInstance(getResources().getString(R.string.activity_help_detail_options),
-                getResources().getString(R.string.activity_help_detail_options_text),
-                R.mipmap.ic_launcher,
-                ContextCompat.getColor(this, R.color.primary)));
-
-        addSlide(AppIntroFragment.newInstance(getResources().getString(R.string.activity_help_favorite),
+        /*addSlide(AppIntroFragment.newInstance(getResources().getString(R.string.activity_help_favorite),
                 getResources().getString(R.string.activity_help_favorite_text),
-                R.mipmap.ic_launcher,
-                ContextCompat.getColor(this, R.color.primary)));
+                R.drawable.preferiti,
+                ContextCompat.getColor(this, R.color.primary)));*/
 
         addSlide(AppIntroFragment.newInstance(getResources().getString(R.string.activity_help_buy),
                 getResources().getString(R.string.activity_help_buy_text),
-                R.mipmap.ic_launcher,
+                R.drawable.intro_comprare,
                 ContextCompat.getColor(this, R.color.primary)));
 
         addSlide(AppIntroFragment.newInstance(getResources().getString(R.string.activity_help_settings),
                 getResources().getString(R.string.activity_help_settings_text),
-                R.mipmap.ic_launcher,
+                R.drawable.intro_impostazioni,
                 ContextCompat.getColor(this, R.color.primary)));
 
         setDoneText(getResources().getText(R.string.dialog_confirm_button));
 
-        setFadeAnimation();
+        setZoomAnimation();
     }
 
     @Override
