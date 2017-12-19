2acb3a267a517f81d751a6e997cbb185498fecb0
FB <francesco.bevilacqua@bepatient.com>
2017-12-19 10:47:49 +0100

    Updates intro images

--- app/src/main/java/org/checklist/comics/comicschecklist/ActivityGuide.java
+++ app/src/main/java/org/checklist/comics/comicschecklist/ActivityGuide.java
@@ -35,28 +35,28 @@ public class ActivityGuide extends AppIntro {
         SliderPage sliderPage2 = new SliderPage();
         sliderPage2.setTitle(getResources().getString(R.string.activity_help_list));
         sliderPage2.setDescription(getResources().getString(R.string.activity_help_list_text));
-        sliderPage2.setImageDrawable(R.drawable.intro_editori);
+        sliderPage2.setImageDrawable(R.drawable.intro_editors);
         sliderPage2.setBgColor(ContextCompat.getColor(this, R.color.primary));
         addSlide(AppIntroFragment.newInstance(sliderPage2));
 
         SliderPage sliderPage3 = new SliderPage();
         sliderPage3.setTitle(getResources().getString(R.string.activity_help_detail));
         sliderPage3.setDescription(getResources().getString(R.string.activity_help_detail_text));
-        sliderPage3.setImageDrawable(R.drawable.intro_dettagli);
+        sliderPage3.setImageDrawable(R.drawable.intro_details);
         sliderPage3.setBgColor(ContextCompat.getColor(this, R.color.primary));
         addSlide(AppIntroFragment.newInstance(sliderPage3));
 
         SliderPage sliderPage4 = new SliderPage();
         sliderPage4.setTitle(getResources().getString(R.string.activity_help_buy));
         sliderPage4.setDescription(getResources().getString(R.string.activity_help_buy_text));
-        sliderPage4.setImageDrawable(R.drawable.intro_comprare);
+        sliderPage4.setImageDrawable(R.drawable.intro_add);
         sliderPage4.setBgColor(ContextCompat.getColor(this, R.color.primary));
         addSlide(AppIntroFragment.newInstance(sliderPage4));
 
         SliderPage sliderPage5 = new SliderPage();
         sliderPage5.setTitle(getResources().getString(R.string.activity_help_settings));
         sliderPage5.setDescription(getResources().getString(R.string.activity_help_settings_text));
-        sliderPage5.setImageDrawable(R.drawable.intro_impostazioni);
+        sliderPage5.setImageDrawable(R.drawable.intro_settings);
         sliderPage5.setBgColor(ContextCompat.getColor(this, R.color.primary));
         addSlide(AppIntroFragment.newInstance(sliderPage5));
 
