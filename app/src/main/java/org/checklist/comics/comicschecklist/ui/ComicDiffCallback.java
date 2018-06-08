bbf4a4761b524090cfe4fed4f092295083dd79e0
FB <francesco.bevilacqua@bepatient.com>
2018-06-08 15:21:52 +0200

    Fixed list updated when comic is added / removed from favorite / cart

---
+++ app/src/main/java/org/checklist/comics/comicschecklist/ui/ComicDiffCallback.java
@@ -0,0 +1,61 @@
+package org.checklist.comics.comicschecklist.ui;
+
+import android.os.Build;
+import android.support.annotation.Nullable;
+import android.support.v7.util.DiffUtil;
+
+import org.checklist.comics.comicschecklist.model.Comic;
+
+import java.util.List;
+import java.util.Objects;
+
+public class ComicDiffCallback extends DiffUtil.Callback {
+
+    private List<Comic> mOldComics;
+    private List<Comic> mNewComics;
+
+    ComicDiffCallback(List<Comic> newComics, List<Comic> oldComics) {
+        this.mNewComics = newComics;
+        this.mOldComics = oldComics;
+    }
+
+    @Override
+    public int getOldListSize() {
+        return mOldComics.size();
+    }
+
+    @Override
+    public int getNewListSize() {
+        return mNewComics.size();
+    }
+
+    @Override
+    public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+        return mOldComics.get(oldItemPosition).getId() ==
+                mNewComics.get(newItemPosition).getId();
+    }
+
+    @Override
+    public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+        Comic newComic = mNewComics.get(newItemPosition);
+        Comic oldComic = mOldComics.get(oldItemPosition);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            return newComic.getId() == oldComic.getId()
+                    && Objects.equals(newComic.isFavorite(), oldComic.isFavorite())
+                    && Objects.equals(newComic.isOnCart(), oldComic.isOnCart())
+                    && Objects.equals(newComic.getReleaseDate(), oldComic.getReleaseDate());
+        } else {
+            return newComic.getId() == oldComic.getId()
+                    && newComic.isFavorite() == oldComic.isFavorite()
+                    && newComic.isOnCart() == oldComic.isOnCart()
+                    && newComic.getReleaseDate().equals(oldComic.getReleaseDate());
+        }
+    }
+
+    @Nullable
+    @Override
+    public Object getChangePayload(int oldItemPosition, int newItemPosition) {
+        // Can return particular field for changed item
+        return super.getChangePayload(oldItemPosition, newItemPosition);
+    }
+}
